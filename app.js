680716a659587686cb137d1c4675c433df5e84ec
Devin Hoole <DevinHoole@outlook.com>
2018-02-14 23:23:17 -0700

    testing jquery for a scrolling panel snap.

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-
-
 var options = {
     $menu: $('header .menu')
 };
 
-$('.docScroller').panelSnap(options);
+$('.panel_container').panelSnap(options);
 
+$('.tester').on('click', function() {
+  var $target = $('.panel_container').panelSnap('getPanel', '.panel2');
+  console.log($target);
+  $('.panel_container').panelSnap('snapToPanel', $target);
+});
