d2f6ea88f471cadc6aa4ecd7b0cba324516e39a6
Devin Hoole <DevinHoole@outlook.com>
2018-02-15 20:36:26 -0700

    Implemented the scrolling panel snap

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var options = {
 
 $('.panel_container').panelSnap(options);
 
-$('.tester').on('click', function() {
-  var $target = $('.panel_container').panelSnap('getPanel', '.panel2');
-  console.log($target);
-  $('.panel_container').panelSnap('snapToPanel', $target);
-});
+//$('.tester').on('click', function() {
+  //var $target = $('.panel_container').panelSnap('getPanel', '.panel2');
+  //console.log($target);
+  //$('.panel_container').panelSnap('snapToPanel', $target);
+//});
